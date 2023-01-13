b1c009d9366276e74ac0be1046f15f9024409319
tommipoiko <tommi.poiko@gmail.com>
2023-01-13 20:34:22 +0200

    4.9 done

--- osa4/blog_list/tests/blog_api.test.js
+++ osa4/blog_list/tests/blog_api.test.js
@@ -4,7 +4,6 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-const { default: test } = require('node:test')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
