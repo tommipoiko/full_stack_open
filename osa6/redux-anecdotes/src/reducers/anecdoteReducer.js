99b9d5ade530de54346316283b1ce81075684888
poikolai <tommi.poikolainen@nokia.com>
2023-02-08 16:28:22 +0200

    6.3 done

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,25 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  console.log(action)
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.payload.id
+      const a = state.find(a => a.id === id)
+      const newA = {
+        ...a,
+        votes: a.votes + 1
+      }
+      return state.map(a => a.id !== id ? a : newA)
+    default: return state
+  }
+}
 
-  return state
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
 }
 
 export default reducer
