e313fea2e960c629b96bea31d47863153c089276
tommipoiko <tommi.poiko@gmail.com>
2023-02-10 18:25:11 +0200

    7.2 done

---
+++ osa7/routed-anecdotes/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useParams,
+  useNavigate,
+  useMatch
+} from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
