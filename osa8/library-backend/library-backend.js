7a167e1f894b036e6273e42330a0f2d23372d188
tommipoiko <tommi.poiko@gmail.com>
2023-02-23 20:23:55 +0200

    8.21 done

--- osa8/library-backend/library-backend.js
+++ osa8/library-backend/library-backend.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken')
 const Author = require('./models/author')
 const Book = require('./models/book')
 const User = require('./models/user')
+const typeDefs = require('./schema')
 
 require('dotenv').config()
 const MONGODB_URI = process.env.MONGODB_URI
@@ -18,62 +19,6 @@ mongoose.connect(MONGODB_URI)
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
-const typeDefs = `
-  type Book {
-    title: String!
-    published: Int!
-    author: Author!
-    genres: [String!]!
-    id: ID!
-  }
-
-  type Author {
-    name: String!
-    born: Int
-    id: ID!
-    bookCount: Int!
-  }
-
-  type User {
-    username: String!
-    favouriteGenre: String!
-    id: ID!
-  }
-  
-  type Token {
-    value: String!
-  }
-
-  type Query {
-    bookCount: Int!,
-    authorCount: Int!
-    allBooks(author: String, genre: String): [Book!]!
-    allAuthors: [Author!]!
-    me: User
-  }
-
-  type Mutation {
-    addBook(
-      title: String!
-      author: String!
-      published: Int!
-      genres: [String!]!
-    ): Book
-    editAuthor(
-      name: String!
-      setBornTo: Int!
-    ): Author
-    createUser(
-      username: String!
-      favouriteGenre: String!
-    ): User
-    login(
-      username: String!
-      password: String!
-    ): Token
-  }
-`
 
 const resolvers = {
   Query: {
