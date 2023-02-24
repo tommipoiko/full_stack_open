0ff015ee450d722ea46266e8cbccd773fe8d06d4
tommipoiko <tommi.poiko@gmail.com>
2023-02-24 16:28:41 +0200

    9.5 done

--- osa9/first-steps/bmiCalculator.ts
+++ osa9/first-steps/bmiCalculator.ts
@@ -3,26 +3,28 @@ interface bmiValues {
   weight: number;
 }
 
-const parseArguments = (args: string[]): bmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+export const parseArguments = (args: string[]): bmiValues => {
+  if (args.length < 2) throw new Error('Not enough arguments');
+  if (args.length > 2) throw new Error('Too many arguments');
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+  if (!isNaN(Number(args[0])) && !isNaN(Number(args[1]))) {
     return {
-      height: Number(args[2]),
-      weight: Number(args[3])
+      height: Number(args[0]),
+      weight: Number(args[1])
     }
   } else {
     throw new Error('Provided values were not numbers!');
   }
 }
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height/100)**2
   if ( bmi > 18.5 && bmi < 24.9 ) {
     return ('Normal (healthy weight)')
   } else if (bmi > 25) {
     return ('Overweight')
+  } else {
+    return ('String so TS is happy')
   }
 }
 
---
+++ osa9/first-steps/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+const app = express();
+import { parseArguments, calculateBmi } from './bmiCalculator';
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const h = String(req.query.height)
+  const w = String(req.query.weight)
+  try {
+    const { height, weight } = parseArguments([h, w])
+    const ret = {
+      weight: weight,
+      height: height,
+      bmi: calculateBmi(height, weight)
+    }
+    res.send(ret);
+  } catch (error) {
+    const ret = {
+      error: 'malformatted parameters'
+    }
+    res.send(ret);
+  }
+});
+
+const PORT = 3002;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
