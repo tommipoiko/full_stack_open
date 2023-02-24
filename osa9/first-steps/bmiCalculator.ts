b95303458cc98e15881b6872d8c6dd3bc56c893c
tommipoiko <tommi.poiko@gmail.com>
2023-02-24 15:34:12 +0200

    9.3 done

--- osa9/first-steps/bmiCalculator.ts
+++ osa9/first-steps/bmiCalculator.ts
@@ -1,7 +1,38 @@
-const calculateBmi = (height: number, mass: number): string => {
-  if ( mass / (height/100)**2 > 18.5 && mass / (height/100)**2 < 24.9 ) {
-    return ('Normal (healthy weight)')
+interface bmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): bmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
 }
 
-console.log(calculateBmi(180, 74))
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height/100)**2
+  if ( bmi > 18.5 && bmi < 24.9 ) {
+    return ('Normal (healthy weight)')
+  } else if (bmi > 25) {
+    return ('Overweight')
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- osa9/first-steps/exerciseCalculator.ts
+++ osa9/first-steps/exerciseCalculator.ts
@@ -1,3 +1,26 @@
+interface exerciseValues {
+  target: number;
+  exerciseDays: number[];
+}
+
+const checkArguments = (args: string[]): exerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  if (!isNaN(Number(args[2]))) {
+    args.slice(3).filter(n => !isNaN(Number(n)))
+    if (args.slice(3).length !== args.slice(3).filter(n => !isNaN(Number(n))).length) {
+      throw new Error('Provided values were not numbers!');
+    }
+    const d = args.slice(3).map(n => Number(n))
+    return {
+      target: Number(args[2]),
+      exerciseDays: d
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 interface Result {
   periodLength: number,
   trainingDays: number,
@@ -11,9 +34,9 @@ interface Result {
 const getRating = (avg: number, target: number): number => {
   const multiplier = avg / target
   let rating = 0
-  if (multiplier > 1.1) {
+  if (multiplier > 1.2) {
     rating = 3
-  } else if (multiplier < 0.9) {
+  } else if (multiplier < 0.8) {
     rating = 1
   } else {
     rating = 2
@@ -50,4 +73,13 @@ const calculateExercises = (dailyExercise: number[], target: number): Result => 
   return ret
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const { target, exerciseDays } = checkArguments(process.argv);
+  console.log(calculateExercises(exerciseDays, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
